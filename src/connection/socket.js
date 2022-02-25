dbe5167742c20bfb9bbcaac3a8dae5b6f39bb7ac
lukasz <lukasz@topsa.com.pl>
2022-02-25 19:10:38 +0100

    init mac

--- src/connection/socket.js
+++ src/connection/socket.js
@@ -1,8 +1,10 @@
 import io from 'socket.io-client'
 
-const URL = 'http://localhost:8000'
+// const URL = 'http://localhost:8000'
+const URL = 'https://app.crmbroker.pl/chess/api/'
 
 const socket = io(URL)
+
 
 var mySocketId
 // register preliminary event listeners here:
